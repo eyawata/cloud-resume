4e8be571a4faaab78f8d5e4b70b5ff717e258b6c
Eri Yawata <yawata.eri@gmail.com>
2025-04-03 17:33:59 +0900

    initialize OIDC

--- terraform/dev-account/main.tf
+++ terraform/dev-account/main.tf
@@ -99,3 +99,41 @@ resource "aws_route53_record" "www_subdomain" {
     evaluate_target_health = false
   }
 }
+
+###############################################################################
+# OIDC and Github Actions Setup
+###############################################################################
+
+# OIDC Identity Provider for GitHub Actions
+resource "aws_iam_openid_connect_provider" "github_oidc" {
+    url             = "https://token.actions.githubusercontent.com"
+    client_id_list  = ["sts.amazonaws.com"]
+    thumbprint_list = ["6938fd4d98bab03faadb97b34396831e3780aea1"] # GitHub OIDC cert thumbprint
+}
+
+# IAM Role for GitHub Actions
+resource "aws_iam_role" "github_actions" {
+    name = var.iam_role_name
+
+    assume_role_policy = jsonencode({
+        Version = "2012-10-17",
+        Statement = [{
+        Effect = "Allow",
+        Principal = {
+            Federated = aws_iam_openid_connect_provider.github_oidc.arn
+        },
+        Action = "sts:AssumeRoleWithWebIdentity",
+        Condition = {
+            StringLike = {
+            "token.actions.githubusercontent.com:sub" = "repo:${var.github_org}/${var.github_repo}:ref:refs/heads/main"
+            }
+        }
+        }]
+    })
+}
+
+# Attach AWS-managed AdministratorAccess policy to Role
+resource "aws_iam_role_policy_attachment" "attach_github_policy" {
+  role       = aws_iam_role.github_actions.name
+  policy_arn = "arn:aws:iam::aws:policy/AdministratorAccess"
+}
--- terraform/dev-account/variables.tf
+++ terraform/dev-account/variables.tf
@@ -14,3 +14,19 @@ variable "hosted_zone_id" {
   description = "The Route53 hosted zone ID"
   type        = string
 }
+
+variable "iam_role_name" {
+  description = "Name of the IAM Role for GitHub Actions"
+  type = string
+  default = "github-actions"
+}
+
+variable "github_org" {
+  description = "GitHub Organization/User name"
+  type        = string
+}
+
+variable "github_repo" {
+  description = "GitHub Repository name"
+  type        = string
+}
